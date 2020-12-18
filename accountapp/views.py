f148cd184302a30d20724d86e4d55d6a3780da14
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-18 18:31:44 +0900

    CreateView를 통한 회원가입 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
-from django.urls import reverse
-
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 from accountapp.models import HelloWorld
-
 
 
 def hello_world(request):
@@ -22,3 +21,17 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    # 어떤 모델을 사용할지
+    model = User
+
+    # 이 User 모델을 만드는데 필요한 form
+    form_class = UserCreationForm
+
+    # 이 계정을 만들기에 성공했다면 어느 경로로 redirection 할 지
+    success_url = reverse_lazy('accountapp:hello_world')
+
+    # template 지정 (어느 html 파일을 이용해서 회원가입 페이지를 보여줄지)
+    template_name = 'accountapp/create.html'
