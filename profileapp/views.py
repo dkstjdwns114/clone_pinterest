5ea39eddecfe456fbdb2b1763d513bb0aeeeb989
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-20 01:41:25 +0900

    profile update 성공시 url 설정 && refactoring

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,9 +14,8 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail')
     template_name = 'profileapp/create.html'
-
 
     def form_valid(self, form):
         # 임시 데이터 저장(DB에는 올리지 않음)
@@ -30,6 +29,11 @@ class ProfileCreateView(CreateView):
         
         return super().form_valid(form)
 
+    # profile update 후 accounts/detail 로 이동시킬껀데
+    # pk 값을 동적으로 받아오기 위해 작성하는 메소드
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -37,5 +41,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
