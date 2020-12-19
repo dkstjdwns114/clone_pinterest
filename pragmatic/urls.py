0b1bcb5ff7f000c52744aca6d90a734d29a0f719
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-20 01:22:29 +0900

    Finish Profileapp

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 위와같이 static을 더해줘야 media 안의 경로를 서버가 인식할 수 있다.
