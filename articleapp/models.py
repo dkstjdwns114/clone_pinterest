a8b551b0b17caacc27794f7a05c5cac9b0ffbbf5
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-21 22:17:48 +0900

    model 수정(article에 project 추가)

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
 
 class Article(models.Model):
     # on_delete=models.SET_NULL : Article이 회원 탈퇴를 했을 때 게시글이 사라지지 않고 작성자명: 알수없음 이런식으로 되게 하는 코드
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
