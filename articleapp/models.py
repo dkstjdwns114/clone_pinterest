232c9f7001d5d7287b8e0f1d473f29fd9855d934
dkstjdwns114 <sungjoon256@gmail.com>
2020-12-21 14:49:58 +0900

    articleapp CRUD 마무리

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    # on_delete=models.SET_NULL : Article이 회원 탈퇴를 했을 때 게시글이 사라지지 않고 작성자명: 알수없음 이런식으로 되게 하는 코드
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    created_at = models.DateField(auto_created=True, null=True)
